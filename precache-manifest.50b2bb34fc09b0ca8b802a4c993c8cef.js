50543ae31c33cea726f6d7afd3151cb41d1360fb
lethanhtruongvinh99 <lethanhtruongvinh99@gmail.com>
2020-10-20 21:13:22 +0700

    Updates

---
+++ precache-manifest.50b2bb34fc09b0ca8b802a4c993c8cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54232d4c07ddf4a9751a50355521b6a0",
+    "url": "/hooktictactoe/index.html"
+  },
+  {
+    "revision": "530b9bc1c2e0e8879471",
+    "url": "/hooktictactoe/static/css/main.91b5816d.chunk.css"
+  },
+  {
+    "revision": "56848c063d0db96c0aa2",
+    "url": "/hooktictactoe/static/js/2.110e820d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hooktictactoe/static/js/2.110e820d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530b9bc1c2e0e8879471",
+    "url": "/hooktictactoe/static/js/main.f154aa40.chunk.js"
+  },
+  {
+    "revision": "5dfc2c6106416707f2ef",
+    "url": "/hooktictactoe/static/js/runtime-main.fa023b7f.js"
+  }
+]);
